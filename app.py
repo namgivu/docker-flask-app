e5bcef6249f2fa3f676e1952472c61cc797cc04f
namgivu <namgivu@gmail.com>
2017-08-25 13:12:23 +0700

    code 0th

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Docker Flask web application'
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True, port=5000)
