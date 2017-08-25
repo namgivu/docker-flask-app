18f98c9e84f85e737bf21683bc728c15d12525f5
namgivu <namgivu@gmail.com>
2017-08-25 15:18:03 +0700

    build image

--- util/00b.list-image.sh
+++ util/00b.list-image.sh
@@ -2,7 +2,6 @@
 
 #get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
-s="$SCRIPT_HOME/.." ; s=$(cd "$s" && pwd) ; export APP_HOME=$s
 
 source "$SCRIPT_HOME/common.sh"
 
