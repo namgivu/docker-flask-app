558a5055b0a46b7c148ab3e3defbb21ff85701df
namgivu <namgivu@gmail.com>
2017-08-25 15:44:36 +0700

    util configure

--- util/common.sh
+++ util/common.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-IMAGE_TAG='nn/docker-flask-app:latest'
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export COMMON_SCRIPT_HOME=$s
+
+#load local common setting if any
+source $COMMON_SCRIPT_HOME/common_local.sh > /dev/null 2>&1
+
+
+IMAGE_NAME='docker-flask-app'
+VERSION_TAG='latest'
+IMAGE_NAME_LOCAL="$IMAGE_NAME:$VERSION_TAG"
+IMAGE_NAME_REMOTE="$DOCKER_HUB_USER/$IMAGE_NAME_LOCAL"
+IMAGE_TAG=$IMAGE_NAME_LOCAL #just an alias
+CONTAINER_NAME="container.$IMAGE_NAME"
+
 HOST_PORT=17825
 CONTAINER_PORT=5000
