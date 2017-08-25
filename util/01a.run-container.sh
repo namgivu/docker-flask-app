97c3f9e4db44b0083c9d5444ee909098b61c12dd
namgivu <namgivu@gmail.com>
2017-08-25 15:14:55 +0700

    run/stop container

---
+++ util/01a.run-container.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
+
+source "$SCRIPT_HOME/common.sh"
+
+docker run -d --name $CONTAINER_NAME -p $HOST_PORT:$CONTAINER_PORT $IMAGE_TAG
---
+++ util/01b.stop-container.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
+
+source "$SCRIPT_HOME/common.sh"
+
+docker rm -f $CONTAINER_NAME
